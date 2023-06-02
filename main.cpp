7f7e0c791ad61f6bb6c6794c32463e64d8692faf
NguyenThacHoangNam_11224467 <117352023+nthnit@users.noreply.github.com>
2023-06-02 08:49:47 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,47 @@
-/******************************************************************************
-Copy đoạn code vào file này
-**Lưu ý không đổi tên file hoặc định dạng file
-**Nộp thành công sẽ có tích xanh ở ngoài repository
-*******************************************************************************/
-#include <stdio.h>
-
-int main()
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct student 
 {
-    printf("Hello World");
-
-    return 0;
+    char name[20];
+    int eng;
+    int math;
+    int phys;
+};
+void print(struct student hocsinh[]){
+    for (int i = 0; i < 4; ++i) {
+        printf("Name: %s\n", hocsinh[i].name);
+        printf("Eng: %d\n", hocsinh[i].eng);
+        printf("Math: %d\n", hocsinh[i].math);
+        printf("Phys: %d\n", hocsinh[i].phys);   
+        grade(hocsinh[i]);
+        printf("\n");
+}
+}
+void grade(struct student hocsinh)
+{
+    float mean;
+    mean = (hocsinh.eng + hocsinh.math + hocsinh.phys) / 3;
+    if (90 <= mean && mean <= 100)
+    {
+        printf("Grade: S");
+    }
+    else if (80 <= mean && mean < 90)
+    {
+        printf("Grade: A");
+    }
+    else if (70 <= mean && mean < 80)
+    {
+        printf("Grade: B");
+    }
+    else if (60 <= mean && mean < 70)
+    {
+        printf("Grade: C");
+    }   
+    else printf("Grade: D");
+};
+int main(){
+    struct student hocsinh[100] = {{"Jack", 82, 72, 58},{"Young", 77, 82, 79},{"Steeve", 52, 62, 39},{"Mark", 61, 82, 88}};
+    print(hocsinh);
+    
 }
