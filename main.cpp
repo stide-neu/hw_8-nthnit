4dc095835b14bb70fd51ea4a597861b47a829dfd
NguyenThacHoangNam_11224467 <117352023+nthnit@users.noreply.github.com>
2023-06-02 09:49:35 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,41 +7,64 @@ struct student
     int eng;
     int math;
     int phys;
+    char grade;
 };
-void print(struct student hocsinh[]){
-    for (int i = 0; i < 4; ++i) {
+void input(struct student hocsinh[]){
+    for (int i = 0; i < 4; i++)
+    {
+        printf("Nhap du lieu cho sinh vien thu %d:\n", i + 1);
+        printf("Ho ten: "); fflush(stdin);
+        gets(hocsinh[i].name);
+        printf("Diem Tieng Anh: "); fflush(stdin);
+        scanf("%d", &hocsinh[i].eng);
+        printf("Diem Toan: "); fflush(stdin);
+        scanf("%d", &hocsinh[i].math);
+        printf("Diem Vat Ly: "); fflush(stdin);
+        scanf("%d", &hocsinh[i].phys);
+    } 
+} 
+void output(struct student hocsinh[])
+    {for (int i = 0; i < 4; ++i) {
         printf("Name: %s\n", hocsinh[i].name);
         printf("Eng: %d\n", hocsinh[i].eng);
         printf("Math: %d\n", hocsinh[i].math);
         printf("Phys: %d\n", hocsinh[i].phys);   
-        grade(hocsinh[i]);
+        printf("Grade: %c", hocsinh[i].grade);
         printf("\n");
+        printf("------------\n");
 }
-}
-void grade(struct student hocsinh)
+    }
+void grade(struct student hocsinh[])
 {
     float mean;
-    mean = (hocsinh.eng + hocsinh.math + hocsinh.phys) / 3;
+    for (int i = 0; i < 4; i++)
+    {
+        mean = (hocsinh[i].eng + hocsinh[i].math + hocsinh[i].phys) / 3;
     if (90 <= mean && mean <= 100)
     {
-        printf("Grade: S");
+        hocsinh[i].grade = 'S';
     }
     else if (80 <= mean && mean < 90)
     {
-        printf("Grade: A");
+        hocsinh[i].grade = 'A';
     }
     else if (70 <= mean && mean < 80)
     {
-        printf("Grade: B");
+        hocsinh[i].grade = 'B';
     }
     else if (60 <= mean && mean < 70)
     {
-        printf("Grade: C");
+        hocsinh[i].grade = 'C';
     }   
-    else printf("Grade: D");
+    else hocsinh[i].grade = 'D';
+
+    }
+    
 };
 int main(){
-    struct student hocsinh[100] = {{"Jack", 82, 72, 58},{"Young", 77, 82, 79},{"Steeve", 52, 62, 39},{"Mark", 61, 82, 88}};
-    print(hocsinh);
+    struct student hocsinh[100] ;
+    input(hocsinh);
+    grade(hocsinh);
+    output(hocsinh);
     
 }
